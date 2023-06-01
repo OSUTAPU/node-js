cd914657a784740238aa1f47c07d2a2f1828467c
OSUTAPU <113910577+OSUTAPU@users.noreply.github.com>
2023-06-01 14:29:32 +0300

    Update get.js

--- get.js
+++ get.js
@@ -1,45 +1,22 @@
 module.exports = (request, response) => {
-    // Отримуємо ссилку без параметрів запиту
-    const url = request.url.split("?")[0];
+  const url = request.url.split("?")[0];
 
-    switch(url) {
-        case "/posts":
-            // Перевіряємо, чи є параметр айді у запиті
-            if (request.query.searchParams.get("id")) {
-                // Отримуємо айдішку з параметрів запиту
-                const id = request.query.searchParams.get("id");
+  if (url === "/posts") {
+    const id = request.query.searchParams.get("id");
 
-                // Встановлюємо статус що все круто
-                response.statusCode = 200;
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
 
-                // Встановлюємо заголовок 
-                response.setHeader("Content-Type", "application/json");
-
-                // Відправляємо відповідь з постом за вказаним ідентифікатором у форматі JSON
-                response.write(JSON.stringify(request.posts[id]));
-
-                response.end();
-            } else {
-                // Встановлюємо статус що все круто
-                response.statusCode = 200;
-
-                // Встановлюємо заголовок 
-                response.setHeader("Content-Type", "application/json");
-
-                // Відправляємо відповідь з усіма постами у форматі JSON
-                response.write(JSON.stringify(request.posts));
-
-                response.end();
-            }
-            break;
-        default:
-            // Встановлюємо статус що все так собі
-            response.statusCode = 400;
-
-            // якщо ресурс не можна отримати, то буде помилка
-            response.write(`CANNOT GET ${request.url}`);
-
-            response.end();
-            break;
+    if (id) {
+      response.write(JSON.stringify(request.posts[id]));
+    } else {
+      response.write(JSON.stringify(request.posts));
     }
+
+    response.end();
+  } else {
+    response.statusCode = 400;
+    response.write(`CANNOT GET ${request.url}`);
+    response.end();
+  }
 };
