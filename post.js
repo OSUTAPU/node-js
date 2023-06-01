f8c5aa0acb26f12762ec95c92b61b2be0852c0b3
OSUTAPU <113910577+OSUTAPU@users.noreply.github.com>
2023-06-01 14:32:31 +0300

    Update post.js

--- post.js
+++ post.js
@@ -1,28 +1,15 @@
 module.exports = (request, response) => {
-  // Вибір дії залежно від ссилки запиту
-  switch (request.url) {
-      case "/posts":
-          // Додавання тіла запиту до масиву постів
-          request.posts.push(request.body);
+  const url = request.url.split("?")[0];
 
-          // Встановлюємо статус що все круто
-          response.statusCode = 200;
-
-          // Встановлення заголовка
-          response.setHeader("Content-Type", "application/json");
-
-          // Відправлення відповіді з усіма постами у форматі JSON
-          response.write(JSON.stringify(request.posts));
-
-          response.end();
-          break;
-      default:
-          // Встановлюємо статус що все так собі
-          response.statusCode = 400;
-
-          // Відправлення повідомлення про помилку, що неможливо зробити POST запит
-          response.write(`CANNOT POST ${request.url}`);
-
-          response.end();
+  if (url === "/posts") {
+    request.posts.push(request.body);
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(request.posts));
+    response.end();
+  } else {
+    response.statusCode = 400;
+    response.write(`CANNOT POST ${request.url}`);
+    response.end();
   }
 };
