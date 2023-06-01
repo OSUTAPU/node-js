d9994c021b35e19454157f0b54d14d2155bc3dbe
OSUTAPU <113910577+OSUTAPU@users.noreply.github.com>
2023-06-01 14:28:33 +0300

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,34 +1,16 @@
 module.exports = (request, response) => {
-  // Отримуємо посилання без параметрів запиту
   const url = request.url.split("?")[0];
 
-  switch (url) {
-    case "/posts":
-      // Отримуємо айдішку з параметрів запиту
-      const id = request.query.searchParams.get("id");
-
-      // Встановлюємо статус що все круто
-      response.statusCode = 200;
-
-      // Встановлюємо заголовок
-      response.setHeader("Content-Type", "application/json");
-
-      // Видаляємо пост з масиву за вказаним айді
-      request.posts.splice(id, 1);
-
-      // Відправляємо відповідь з масивом у форматі JSON
-      response.write(JSON.stringify(request.posts));
-
-      response.end();
-      break;
-    default:
-      // Встановлюємо статус що все так собі
-      response.statusCode = 400;
-
-      // якщо ресурс не можна видалити, то буде помилка
-      response.write(`CANNOT DELETE ${request.url}`);
-
-      response.end();
-      break;
+  if (url === "/posts" && request.query.searchParams.has("id")) {
+    const id = request.query.searchParams.get("id");
+    request.posts.splice(id, 1);
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(request.posts));
+    response.end();
+  } else {
+    response.statusCode = 400;
+    response.write(`CANNOT DELETE ${request.url}`);
+    response.end();
   }
 };
