6a5c1ad7fb5cf6a26bbbe26c9465cd2120b851a9
OSUTAPU <113910577+OSUTAPU@users.noreply.github.com>
2023-06-01 14:31:44 +0300

    Update getBody.js

--- getBody.js
+++ getBody.js
@@ -1,26 +1,17 @@
-const fs = require("fs");
-
 module.exports = (request, response, next) => {
-    // Створення порожнього масиву для зберігання даних
-    let data = [];
+  let data = [];
 
-    // Обробник події "data"
-    request.on("data", dataChunk => {
-        // Додавання отриманих даних до масиву
-        data.push(dataChunk);
-    });
+  request.on("data", (dataChunk) => {
+    data.push(dataChunk);
+  });
 
-    // Обробник події "end"
-    request.on("end", () => {
-        // Об'єднання всіх отриманих даних і конвертація у рядок
-        request.body = Buffer.concat(data).toString();
+  request.on("end", () => {
+    request.body = Buffer.concat(data).toString();
 
-        // Перевірка заголовка
-        if (request.headers["content-type"] === "application/json") {
-            // Парсинг тіла запиту з формату JSON у об'єкт
-            request.body = JSON.parse(request.body);
-        }
+    if (request.headers["content-type"] === "application/json") {
+      request.body = JSON.parse(request.body);
+    }
 
-        next(request, response);
-    });
+    next(request, response);
+  });
 };
